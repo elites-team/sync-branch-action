9806d98113a57d787a7d89a768f46533c9daba98
noopy0415 <noopy0415@gmail.com>
2021-01-28 15:22:14 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,16 @@
 
 set -e
 
+git config --local user.email "$GITHUB_RUN_NUMBER+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+git config --local merge.ours.name = "Keep ours merge"
+git config --local merge.ours.driver "true"
 git config -l
+
 git switch master
 
 BRANCHES=`git branch | tr -d '*' | sed 's/^  *//g'` 
 BRANCHES_LENGTH=`git branch | tr -d '*' | sed 's/^  *//g' | wc -l`
-echo "*** loop start ***"
 for i in `seq $BRANCHES_LENGTH`
 do
   BRANCH=`echo $BRANCHES | cut -d ' ' -f $i`

ba21579f709dfa923a058489352e5794848155c5
noopy0415 <noopy0415@gmail.com>
2021-01-28 11:56:51 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,15 @@ git config --global merge.ours.name = "Keep ours merge"
 git config --global merge.ours.driver "true"
 git config -l
 # git switch master
-git fetch
+# git fetch
 
 BRANCHES=`git branch -r` # for main
 # BRANCHES='origin/master origin/php/php' # for develop
 BRANCHES_LENGTH=`echo "$BRANCH " | tr ' ' '\n' | wc -l`
 for i in `seq $BRANCHES_LENGTH`
 do
+  R_BRANCH=`echo $BRANCHES | cut -d ' ' -f $i`
   BRANCH=`echo $BRANCHES | cut -d ' ' -f $i` | tr 'origin/'
+  echo "$R_BRANCH"
   echo "$BRANCH"
 done

06ba1e1a8f3748cc85070f0c0a9ac32fb4003c86
noopy0415 <noopy0415@gmail.com>
2021-01-28 15:20:41 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-git config --global user.email "$GITHUB_RUN_NUMBER+github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
-git config --global merge.ours.name = "Keep ours merge"
-git config --global merge.ours.driver "true"
-# git config -l
+git config -l
 git switch master
 
 BRANCHES=`git branch | tr -d '*' | sed 's/^  *//g'` 
@@ -15,9 +11,8 @@ echo "*** loop start ***"
 for i in `seq $BRANCHES_LENGTH`
 do
   BRANCH=`echo $BRANCHES | cut -d ' ' -f $i`
-  # echo "*** check point ***"
   if [ $BRANCH != 'master' -a $BRANCH != 'action' ]; then
-    # echo $BRANCH
     git switch "${BRANCH}"
+    git merge master
   fi
 done

cee95bdce66874d82136da5cc9f2a2879bde731d
noopy0415 <noopy0415@gmail.com>
2021-01-28 11:36:15 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,19 @@ git config --global user.email "$GITHUB_RUN_NUMBER+github-actions[bot]@users.nor
 git config --global user.name "github-actions[bot]"
 git config --global merge.ours.name = "Keep ours merge"
 git config --global merge.ours.driver "true"
-# git config -l
+git config -l
 # git switch master
-git branch -r
-echo "----- branches=${git branch -r} -----------"
-branches="${git branch -r}"
-echo "----- echo ${branches[@]} --------------"
-echo $(branches[@])
-for branch in ${branches[@]}; do
-  echo branch
+# git branch -r
+echo "----- branches -----------"
+BRANCHES=`git branch -r` # for main
+# BRANCHES='master php/php' # for develop
+BRANCHES_LENGTH=`echo $BRANCH | tr ' ' '\n' | wc -l`
+echo "----- echo \$BRANCHES --------------"
+echo "branch_array : $BRANCHES"
+echo "branch_length : $BRANCHES_LENGTH"
+echo `seq $BRANCHES_LENGTH`
+for i in `seq $BRANCHES_LENGTH`
+do
+  ELEMENT=`echo $BRANCHES | cut -d ' ' -f $i`
+  echo "$ELEMENT"
 done

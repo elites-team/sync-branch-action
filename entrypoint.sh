089532cb042ed61fa19886bc987242bf82248b07
noopy0415 <noopy0415@gmail.com>
2021-01-28 11:55:09 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ git config --global user.name "github-actions[bot]"
 git config --global merge.ours.name = "Keep ours merge"
 git config --global merge.ours.driver "true"
 git config -l
-git switch master
+# git switch master
 git fetch
 
 BRANCHES=`git branch -r` # for main
